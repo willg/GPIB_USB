b40c217b1dc42430f7bd0bfdf23fae8d5e32750b
will <20484529+willg@users.noreply.github.com>
2020-11-15 14:30:40 +0900

    switch from v-scores to mouse bites

--- scripts/panelize.sh
+++ scripts/panelize.sh
@@ -4,7 +4,9 @@ kikit panelize grid \
     --sourcearea 90 60 100 100 \
     --space 2.5 \
     --gridsize 4 1 \
-    --tabwidth 50 \
-    --vcuts \
+    --mousebites 0.5 1 0 \
+    --tabwidth 3 \
+    --tabheight 2.5 \
+    --vtabs 3 \
     --radius 1 \
     adapter.kicad_pcb adapter-panel.kicad_pcb
